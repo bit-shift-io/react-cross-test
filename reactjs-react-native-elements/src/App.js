ce174017f77978945f3fac96602930d6f7f98044
Fabian Mathews <supagu@gmail.com>
2022-05-11 13:06:48 +0930

    some examples for react-native-elements

--- reactjs-react-native-elements/src/App.js
+++ reactjs-react-native-elements/src/App.js
@@ -1,26 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@rneui/base'; // this causes an error re react-native-safe-area-context
+import { ThemeProvider, Button, createTheme } from '@rneui/themed';
+import { AirbnbRating } from "@rneui/themed";
+import {View } from 'react-native'
+import { Chip } from '@rneui/themed';
+import { Switch } from "@rneui/themed";
+
+const theme = createTheme({
+  Button: {
+    titleStyle: {
+      color: 'red',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Button title="Hello World" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <style type="text/css">{`
+        @font-face {
+          font-family: 'MaterialIcons';
+          src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+        }
+
+        @font-face {
+          font-family: 'FontAwesome';
+          src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
+        }
+      `}</style>
+
+      <View style={{padding: '10px'}}>
+        <Button title="Hello World" titleStyle={{ color: 'pink' }} />
+        <AirbnbRating />
+        <Chip
+          title="Pressable Icon Chip"
+          icon={{
+            name: 'close',
+            type: 'font-awesome',
+            size: 20,
+            //color: colors.primary2,
+          }}
+          onPress={() => console.log('Icon chip was pressed!')}
+          iconRight
+          type="outline"
+          containerStyle={{ marginVertical: 15 }}
+        />
+
+        <Switch
+          value={false}
+          //onValueChange={(value) => setChecked(value)}
+        />
+
+      </View>
+    </ThemeProvider>
   );
 }
 
