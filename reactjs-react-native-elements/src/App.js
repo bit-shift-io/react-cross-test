6192a192c056f3d8fd4606e2085718e4da00564f
Fabian Mathews <supagu@gmail.com>
2022-05-13 16:46:52 +0930

    some initial navigation support

--- reactjs-react-native-elements/src/App.js
+++ reactjs-react-native-elements/src/App.js
@@ -8,6 +8,7 @@ import {createReduxModule, Provider} from 'hooks-for-redux'
 import {FlexRow, FlexCol, View, Text} from './components-core'
 import {LoginForm, TodoList} from './components'
 import {styleString} from './utils/style'
+import {Navigation} from './navigation'
 
 const theme = createTheme({
   Button: {
@@ -17,63 +18,68 @@ const theme = createTheme({
   },
 });
 
+// https://reactnavigation.org/docs/web-support
+
 function App() {
   return (
     <Provider>
       <ThemeProvider theme={theme}>
-        <style type="text/css">{`
-          @font-face {
-            font-family: 'MaterialIcons';
-            src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
-          }
+          <style type="text/css">{`
+            @font-face {
+              font-family: 'MaterialIcons';
+              src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+            }
 
-          @font-face {
-            font-family: 'FontAwesome';
-            src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
-          }
-        `}</style>
+            @font-face {
+              font-family: 'FontAwesome';
+              src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
+            }
+          `}</style>
 
-        <View style={{padding: '10px'}}>
-          <Button title="Hello World" buttonStyle={styleString(`br-40px bg-purple`)} titleStyle={styleString(`c-red`)} />
-          <AirbnbRating />
-          <Chip
-            title="Pressable Icon Chip"
-            icon={{
-              name: 'close',
-              type: 'font-awesome',
-              size: 20,
-              //color: colors.primary2,
-            }}
-            onPress={() => console.log('Icon chip was pressed!')}
-            iconRight
-            type="outline"
-            containerStyle={{ marginVertical: 15 }}
-          />
+          <View style={{padding: '10px'}}>
+            <Button title="Hello World" buttonStyle={styleString(`br-40px bg-purple`)} titleStyle={styleString(`c-red`)} />
+            <AirbnbRating />
+            <Chip
+              title="Pressable Icon Chip"
+              icon={{
+                name: 'close',
+                type: 'font-awesome',
+                size: 20,
+                //color: colors.primary2,
+              }}
+              onPress={() => console.log('Icon chip was pressed!')}
+              iconRight
+              type="outline"
+              containerStyle={{ marginVertical: 15 }}
+            />
 
-          <Switch
-            value={false}
-            //onValueChange={(value) => setChecked(value)}
-          />
+            <Switch
+              value={false}
+              //onValueChange={(value) => setChecked(value)}
+            />
 
-          <View bg-red p-2 m-10px br-10px bc-green bw-2px h-20pct>
-            <Text>Styled View Test</Text>
-          </View>
+            <View bg-red p-2 m-10px br-10px bc-green bw-2px h-20pct>
+              <Text>Styled View Test</Text>
+            </View>
 
-          <FlexRow bg-yellow>
-            <FlexCol fh-center bg-orange f-1>
-              <Text>A1</Text>
-              <Text>A2</Text>
-            </FlexCol>
-            <FlexRow f-1>
-              <Text f-1>B1</Text>
-              <Text f-1>B2</Text>
+            <FlexRow bg-yellow>
+              <FlexCol fh-center bg-orange f-1>
+                <Text>A1</Text>
+                <Text>A2</Text>
+              </FlexCol>
+              <FlexRow f-1>
+                <Text f-1>B1</Text>
+                <Text f-1>B2</Text>
+              </FlexRow>
             </FlexRow>
-          </FlexRow>
 
-          <LoginForm/>
-          <TodoList/>
+            <LoginForm/>
+            <TodoList/>
 
-        </View>
+          </View>
+        
+        <Navigation/>
+
       </ThemeProvider>
     </Provider>
   );
---
+++ reactjs-react-native-elements/src/navigation/navigation.js
@@ -0,0 +1,36 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {NavigationContainer} from '@react-navigation/native'
+import {View, Text} from '../components-core'
+import {Link} from '@react-navigation/native'
+
+function LoginScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Login Screen</Text>
+        <Link to={{ screen: 'Details', params: { id: 'jane' } }}>
+            Goto details
+        </Link>
+      </View>
+    );
+}
+
+function DetailsScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+
+const Stack = createNativeStackNavigator()
+
+export const Navigation = (props) => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
